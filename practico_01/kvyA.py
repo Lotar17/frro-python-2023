9ee6fe444f089cd578fcce0800abe0a1348b2b91
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-06-03 19:14:05 -0300

    Realizar hasta el ejercicio 6 de la practica 3

--- practico_01/kvyA.py
+++ practico_01/kvyA.py
@@ -1,2 +1,19 @@
 import kivy
-kivy.require
+from kivy.app import App
+status = 0
+def muestraNumero(scrtext: str, nmbtext: str, status) :
+    if status == 1:
+        scrtext = nmbtext
+        status = 0
+    try:
+        eval(scrtext+nmbtext)
+    except:
+        status = 1
+    else:
+        scrtext += nmbtext
+class buttonApp(App):
+    pass
+
+if __name__ == '__main__':
+    buttonApp().run()
+
