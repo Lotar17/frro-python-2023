4a601f0a262c3a69bca71a2e83e3a054f0871b71
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2023-06-16 13:46:39 -0300

    Continuar formulario en kivy

--- Proyectos Kivy/formulario/main.py
+++ Proyectos Kivy/formulario/main.py
@@ -1,5 +1,9 @@
 import kivy
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.progressbar import ProgressBar
+
 
 class FormularioApp(App):
     pass
