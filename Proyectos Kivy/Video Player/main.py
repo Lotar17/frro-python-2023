155cf4212c0e183e5b6a37bd4b98b41a593e873b
Mateo Rindello <mateorindello14@gmail.com>
2023-07-13 20:11:50 -0300

    imagen ok, ruta ok, avance listview
    cambios para listview, cambios en la ruta y generación de imagen

--- Proyectos Kivy/Video Player/main.py
+++ Proyectos Kivy/Video Player/main.py
@@ -1,36 +1,60 @@
-
 import kivy
 from kivy.app import App
 from kivy.uix import videoplayer
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.recycleview import RecycleView
 from kivy.uix.videoplayer import VideoPlayer
 from database import VideoDAO
+from pathlib import Path
+
+
+class VideoItem(BoxLayout):
+    def __init__(self, video, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'horizontal'
+        self.size_hint_y = None
+        self.height = 100
+        self.video = video
+
+
+class VideoList(RecycleView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.data = []
+
+    def refresh_view_attrs(self, rv, index, data):
+        video = data['video']
+        return {'video': video}
 
 
 class VideoPlayerApp(App):
 
-
-
-
+    def defaultPath(self):
+        path = Path.home() / "documents" / "videoapp"
+        path.mkdir(parents=True, exist_ok=True)
+        return str(path)
 
     def findAll(self):
         videoDB = VideoDAO()
         videos = videoDB.listar()
         return videos
 
-    def generar_grilla(self, grid : GridLayout, button : Button):
-        count = self.findAll().count()
-        for i in range(count):
-            grid.add_widget(button)
-        return grid
+    def generar_grilla(self, video_list: VideoList):
+        videos = self.findAll()
+        count = videos.count()
+        for video in videos:
+            video_item = VideoItem(video=video)
+            video_list.data.append({'video': video_item})
 
     def videoPlayer(self):
-        player = VideoPlayer(source = "Videos TPI/rick-astley.mp4")
+        player = VideoPlayer(source="Videos TPI/rick-astley.mp4")
         player.state = "play"
         player.options = {"eos": "loop"}
         player.alow_stretch = True
         return player
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     VideoPlayerApp().run()
--- Proyectos Kivy/Video Player/video.py
+++ Proyectos Kivy/Video Player/video.py
@@ -1,14 +1,19 @@
+from moviepy.editor import VideoFileClip
+from PIL import Image as Image
 
 
 class Video:
-
-    def __init__(self, nombre = None, descripcion = None, propietario = None, route = None):
+    def __init__(self, nombre=None, descripcion=None, propietario=None, route=None):
         self.nombre = nombre
         self.descripcion = descripcion
         self.propietario = propietario
         self.route = route
+        self.thumb = self.setThumbnail()
 
-
-
-
-
+    def setThumbnail(self):
+        clip = VideoFileClip(self.route)
+        frame = clip.get_frame(10)
+        clip.close()
+        thumbnail = Image.fromarray(frame)
+        thumbnail = thumbnail.resize((320, 240), reducing_gap=True)
+        return thumbnail
